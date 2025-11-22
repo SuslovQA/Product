0a9dae65f1fc9b3da28e1cc0f93e71afba5e32e9
Evgeny <evgenijsuslov08683@gmail.com>
2025-11-23 00:07:23 +0500

    delete some more comments

--- src/main/java/org/example/ProductManager/Manager.java
+++ src/main/java/org/example/ProductManager/Manager.java
@@ -11,20 +11,6 @@ public class Manager {
         repo.saveProduct(product);
     }
 
-//    public boolean matches(Product product, String search) {
-////        if (product.getName().contains(search)) {
-////            return true;
-////        } else {
-////            return false;
-////        }
-//       return product.getName().contains(search);
-//    }
-
-
-
-    //Замените в менеджере вызов метода matches из класса самого менеджера на вызов метода matches у самих продуктов:
-// if (product.matches(search)) {.
-
     public Product[] searchBy(String text) {
         Product[] result = new Product[0];
         for (Product product : repo.getProducts()) {
